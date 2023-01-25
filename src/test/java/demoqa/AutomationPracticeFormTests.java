440629ea6354f6ac57d5d5c3721c0b0844c1d5da
Nikita <sourbeanpie@gmail.com>
2023-01-25 13:38:28 +0300

    added homework lesson4

---
+++ src/test/java/demoqa/AutomationPracticeFormTests.java
@@ -0,0 +1,61 @@
+package demoqa;
+
+import com.codeborne.selenide.*;
+import org.junit.jupiter.api.*;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class AutomationPracticeFormTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
+    @Test
+    void fillAutomationPracticeFormTest() {
+        String name = "test";
+        String lastName = "lastTest";
+        String email = "test@mail.com";
+        String gender = "Male";
+        String number = "0123456789";
+        String birth = "05 October,2000";
+        String hobbies = "Music";
+        String address = "Test Address";
+
+
+        open("/automation-practice-form");
+        $(".main-header").shouldHave(text("Practice Form"));
+
+        $("#firstName").setValue(name);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(email);
+        $("#genterWrapper").$(byText(gender)).click();
+        $("#userNumber").setValue(number);
+
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-select").selectOption("2000");
+        $(".react-datepicker__month-select").selectOption("October");
+        $(".react-datepicker__day--005").click();
+
+        $("#hobbiesWrapper").$(byText(hobbies)).click();
+        $("#uploadPicture").uploadFromClasspath("test.png");
+        $("#currentAddress").setValue(address);
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Noida")).click();
+        $("#submit").click();
+
+        $(".modal-content").shouldBe(visible);
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+
+        $(".modal-body").shouldHave(text(name + " " + lastName), (text(email)),
+                (text(number)), (text(address)), (text(gender)),
+                (text(birth)), (text(hobbies)), (text("test.png")), (text("NCR Noida")));
+    }
+}
